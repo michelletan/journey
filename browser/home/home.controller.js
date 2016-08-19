74a3f13e8b10271c3d10439c0428630ead60de89
superburrito <chuayaohui@gmail.com>
2016-08-19 13:08:55 +0800

    combined states

--- browser/home/home.controller.js
+++ browser/home/home.controller.js
@@ -4,5 +4,4 @@
 // Both HomeController and CountryController use the CountryFactory
 app.controller('HomeCtrl', function($scope, $state, $stateParams, CountryFactory){
 	$scope.countries = countries; 
-
 });
--- browser/landing/landing.controller.js
+++ browser/landing/landing.controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
 app.controller('LandingCtrl', function($scope, $state, $stateParams) {
-
     $scope.imagePath = 'http://dl.dropboxusercontent.com/s/9x8tfh5hy8go4wk/landing-photo.jpg?dl=0'; // placeholder
-
 });
