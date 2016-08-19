8eb5a4030ba1a22da2d00d0a4517055be2f8968e
Michelle Tan <michelletanjm@gmail.com>
2016-08-19 17:01:37 +0800

    Added favicons

--- browser/landing/landing.controller.js
+++ browser/landing/landing.controller.js
@@ -2,4 +2,5 @@
 
 app.controller('LandingCtrl', function($scope, $state, $stateParams) {
     $scope.imagePath = 'http://dl.dropboxusercontent.com/s/9x8tfh5hy8go4wk/landing-photo.jpg?dl=0'; // placeholder
+
 });
