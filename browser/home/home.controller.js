5b25fbff42c0fd1b2d350a1e31f4e26aca218b93
superburrito <chuayaohui@gmail.com>
2016-08-19 22:49:54 +0800

    added db.js and fixed treeflipper

--- browser/home/home.controller.js
+++ browser/home/home.controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/* global countries */
 // Both HomeController and CountryController use the CountryFactory
 app.controller('HomeCtrl', function($scope, $state, $stateParams, CountryFactory, facebookFactory, pixabayFactory, $q){
 	facebookFactory.getCountries()
@@ -17,5 +16,5 @@ app.controller('HomeCtrl', function($scope, $state, $stateParams, CountryFactory
 			$scope.countries = updatedCountries;
 		})
 	});
-	
+
 });
