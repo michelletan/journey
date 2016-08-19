27d5452c35101502106aecc0a27550353131ee66
superburrito <chuayaohui@gmail.com>
2016-08-19 14:09:13 +0800

    changed fbapi.js name

--- browser/navbar/navbar.controller.js
+++ browser/navbar/navbar.controller.js
@@ -8,24 +8,23 @@ app.controller('NavbarCtrl', function($scope, $mdSidenav, facebookFactory) {
     name: 'My Journeys'
   }];
 
-	  console.log("testing");
-    FB.init({
-      appId      : '327010674354140',
-      cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v2.7' // use graph api version 2.7
-    });
-	  console.log("testing2");
+  FB.init({
+    appId      : '327010674354140',
+    cookie     : true,  // enable cookies to allow the server to access 
+                      // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.7' // use graph api version 2.7
+  });
 
-    FB.getLoginStatus(function(response) {
-      facebookFactory.statusChangeCallback(response)
-      .then(function(nameSourceObj){
-        console.log("name and profile pic is: ", nameSourceObj);
-        $scope.name = nameSourceObj.name;
-        $scope.userPic = nameSourceObj.source;
-      })
-    });
+  FB.getLoginStatus(function(response) {
+    facebookFactory.statusChangeCallback(response)
+    .then(function(nameSourceObj){
+      console.log("name and profile pic is: ", nameSourceObj);
+      $scope.name = nameSourceObj.name;
+      $scope.userPic = nameSourceObj.source;
+    })
+  });
+
 
 
 /*
--- browser/navbar/navbar.directive.js
+++ browser/navbar/navbar.directive.js
@@ -4,11 +4,6 @@ app.directive('navbar', function($state) {
 	return {
 		restrict: 'E',
 		templateUrl: '/browser/navbar/navbar.html',
-		link: function(scope){
-			//Todo: $scope of navbar?
-			scope.goToHome = function () {
-				$state.go('home');
-			}
-		}
+		controller: 'NavbarCtrl'
 	}
 });
