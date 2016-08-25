1e0a6f12bb54915c89825b7acee96f63bff8e071
Michelle Tan <michelletanjm@gmail.com>
2016-08-25 17:46:35 +0800

    Added friends, minor changes

--- browser/friends/friends.controller.js
+++ browser/friends/friends.controller.js
@@ -1,5 +1,12 @@
 'use strict';
 
 app.controller('FriendsCtrl', function($scope, $state, $stateParams) {
+    $scope.defaultUserId = 1;
+    $scope.defaultUserPic = '/images/user.png';
+	$scope.defaultUserName = 'User';
+
+    $scope.friends = [
+        {}, {}, {}, {}, {}, {}
+    ];
 
 });
--- browser/journey.states.js
+++ browser/journey.states.js
@@ -10,40 +10,61 @@ app.config(function($stateProvider){
   $stateProvider.state('home', {
     url: '/home',
     templateUrl: '/browser/home/home.html',
-    controller: 'HomeCtrl'
+    controller: 'HomeCtrl',
+    resolve: {
+      $title: function() { return 'Home'; }
+    }
   });
 
   $stateProvider.state('profile', {
     url: '/profile/:id',
     templateUrl: '/browser/profile/profile.html',
-    controller: 'ProfileCtrl'
+    controller: 'ProfileCtrl',
+    resolve: {
+      $title: function() { return 'Profile'; }
+    }
   });
 
   $stateProvider.state('friends', {
     url: '/friends',
     templateUrl: '/browser/friends/friends.html',
-    controller: 'FriendsCtrl'
+    controller: 'FriendsCtrl',
+    resolve: {
+      $title: function() { return 'Friends'; }
+    }
   });
 
   $stateProvider.state('journeys', {
     url: '/journeys',
     templateUrl: '/browser/journeys/journeys.html',
-    controller: 'JourneysCtrl'
+    controller: 'JourneysCtrl',
+    resolve: {
+      $title: function() { return 'Journeys'; }
+    }
   });
 
   $stateProvider.state('policy', {
     url: '/policy',
-    templateUrl: '/browser/policy/policy.html'
+    templateUrl: '/browser/policy/policy.html',
+    resolve: {
+      $title: function() { return 'Privacy Policy'; }
+    }
   });
 
   $stateProvider.state('terms', {
     url: '/terms',
-    templateUrl: '/browser/terms/terms.html'
+    templateUrl: '/browser/terms/terms.html',
+    resolve: {
+      $title: function() { return 'Terms of Service'; }
+    }
   });
 
   $stateProvider.state('credits', {
     url: '/credits',
-    templateUrl: '/browser/credits/credits.html'
+    templateUrl: '/browser/credits/credits.html',
+    resolve: {
+      $title: function() { return 'Credits'; }
+    }
   });
 
 
