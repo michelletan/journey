75ca0de1741500bb783770cfb22ff21a54b9a557
Michelle Tan <michelletanjm@gmail.com>
2016-08-26 19:01:07 +0800

    Added profile page

--- browser/profile/profile.controller.js
+++ browser/profile/profile.controller.js
@@ -2,4 +2,22 @@
 
 app.controller('ProfileCtrl', function($scope, $state, $stateParams) {
     $scope.profileId = $stateParams.id;
+
+    $scope.defaultUserId = 1;
+    $scope.defaultUserPic = '/images/user.png';
+    $scope.defaultUserName = 'User';
+    $scope.defaultCoverUrl = '/images/landing-feature2.jpg';
+
+    $scope.defaultJourneyTitle = 'Amazing Trip';
+
+    $scope.journeys = [
+        {}, {}, {}, {}, {}, {}, {}, {}
+    ];
+
+    $scope.goToPlace = goToPlace;
+
+    // Public functions
+    function goToPlace(id) {
+        $state.go('place', {countryId: $stateParams.id, placeId: id});
+    }
 });
