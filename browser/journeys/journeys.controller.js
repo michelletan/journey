038b119bca61f2fcc150ab915ab602ebd785f29a
superburrito <chuayaohui@gmail.com>
2016-08-29 00:19:20 +0800

    git commit -m

--- browser/journeys/journeys.controller.js
+++ browser/journeys/journeys.controller.js
@@ -1,5 +1,10 @@
 'use strict';
 
-app.controller('JourneysCtrl', function($scope, $state, $stateParams) {
+app.controller('JourneysCtrl', function($scope, $state, $stateParams, DatabaseFactory){
+	$scope.userId = 123; // Placeholder, we need the actual user's facebook ID to get journeys
+	DatabaseFactory.getAllJourneys($scope.userId)
+	.then(function(journeys){
+		$scope.journeys = journeys;
+	});
 
 });
