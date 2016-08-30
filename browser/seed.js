a7b64f12b5528cbb2a85bf2ac9db5c86bfa10ff2
Ten Zhi Yang <tzyinc@hotmail.com>
2016-08-30 12:17:54 +0800

    updated seed file

--- browser/seed.js
+++ browser/seed.js
@@ -1,58 +1,72 @@
 var journeyArrBasic = [
   {
-	"name": "Singapore",
-	"posts": [
-  	{
-    	"id": "1144958495559554_1147631028625634",
-    	"time": "2016-08-26T04:04:38+0000",
-    	"story": "Ten Zhi-Yang at National University of Singapore.",
-    	"message": "teleport back to nus",
-    	"likes": 0,
-    	"country": "Singapore"
-  	}
-	]
+    "name": "My Journey in Singapore",
+    "posts": [
+      {
+        "id": "1144958495559554_1147631028625634",
+        "time": "2016-08-26T04:04:38+0000",
+        "story": "Ten Zhi-Yang at National University of Singapore.",
+        "message": "teleport back to nus",
+        "likes": 0,
+        "country": "Singapore"
+      }
+    ]
   },
   {
-	"name": "Malaysia",
-	"posts": [
-  	{
-    	"id": "1144958495559554_1147630815292322",
-    	"time": "2016-08-26T04:04:19+0000",
-    	"story": "Ten Zhi-Yang at Kuching Museum.",
-    	"message": "cat museum",
-    	"likes": 0,
-    	"country": "Malaysia"
-  	}
-	]
+    "name": "My Journey in Malaysia",
+    "posts": [
+      {
+        "id": "1144958495559554_1147630815292322",
+        "time": "2016-08-26T04:04:19+0000",
+        "story": "Ten Zhi-Yang at Kuching Museum.",
+        "message": "cat museum",
+        "likes": 0,
+        "country": "Malaysia"
+      }
+    ]
   },
   {
-	"name": "Singapore",
-	"posts": [
-  	{
-    	"id": "1144958495559554_1146911085364295",
-    	"time": "2016-08-25T11:36:42+0000",
-    	"story": "Ten Zhi-Yang with Ng Xiao Hui at Saveur at Cathay.",
-    	"message": "Test data",
-    	"src": "https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/14088595_1146911065364297_6541244813022274907_n.jpg?oh=2f6b123962ba195f332015ae0fd568b2&oe=584AA9ED",
-    	"likes": 0,
-    	"country": "Singapore"
-  	},
-  	{
-    	"id": "1144958495559554_1145877815467622",
-    	"time": "2016-08-24T10:37:21+0000",
-    	"story": "Ten Zhi-Yang at NUS School of Computing.",
-    	"message": "test",
-    	"likes": 5,
-    	"country": "Singapore"
-  	},
-  	{
-    	"id": "1144958495559554_1136479309740806",
-    	"time": "2016-08-14T05:06:21+0000",
-    	"story": "Ten Zhi-Yang at Ah Hoe Mee Pok - 阿和麺薄 - Food Stall.",
-    	"message": "Literally the best bak chor mee I ever had. Ng Xiao Hui Yuan Shan Chan Qin Ying",
-    	"likes": 1,
-    	"country": "Singapore"
-  	}
-	]
+    "name": "My Journey in Singapore",
+    "posts": [
+      {
+        "id": "1144958495559554_1146911085364295",
+        "time": "2016-08-25T11:36:42+0000",
+        "story": "Ten Zhi-Yang with Ng Xiao Hui at Saveur at Cathay.",
+        "message": "Test data",
+        "src": "https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/14088595_1146911065364297_6541244813022274907_n.jpg?oh=2f6b123962ba195f332015ae0fd568b2&oe=584AA9ED",
+        "likes": 0,
+        "country": "Singapore"
+      },
+      {
+        "id": "1144958495559554_1145877815467622",
+        "time": "2016-08-24T10:37:21+0000",
+        "story": "Ten Zhi-Yang at NUS School of Computing.",
+        "message": "test",
+        "likes": 5,
+        "country": "Singapore"
+      },
+      {
+        "id": "1144958495559554_1136479309740806",
+        "time": "2016-08-14T05:06:21+0000",
+        "story": "Ten Zhi-Yang at Ah Hoe Mee Pok - 阿和麺薄 - Food Stall.",
+        "message": "Literally the best bak chor mee I ever had. Ng Xiao Hui Yuan Shan Chan Qin Ying",
+        "likes": 1,
+        "country": "Singapore"
+      }
+    ]
+  },
+  {
+    "name": "My Journey in Australia",
+    "posts": [
+      {
+        "id": "1144958495559554_983156741739731",
+        "time": "2015-12-28T03:23:55+0000",
+        "story": "Ten Zhi-Yang in Glenelg, South Australia.",
+        "message": "Seems like he's still forgotten",
+        "src": "https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/12400805_983156728406399_2996096813089968650_n.jpg?oh=8da195bf75c3ec1790d130eb94473367&oe=584C1595",
+        "likes": 2,
+        "country": "Australia"
+      }
+    ]
   }
 ]
