36edb67b0df7727f5c2eb350a1f64cc5e7d1a07b
Michelle Tan <michelletanjm@gmail.com>
2016-08-31 16:40:31 +0800

    Fix scroll container hiding contents issue

--- browser/journey-create/journey-create.controller.js
+++ browser/journey-create/journey-create.controller.js
@@ -3,6 +3,8 @@
 app.controller('JourneyCreateCtrl', function($scope, $state, $stateParams) {
     $scope.defaultPostPic = '/images/landing-feature2.jpg';
 
+    $scope.todayDate = new Date();
+
     $scope.countries = [{}, {}, {}];
 
     $scope.posts = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
