73b8e218ed0d250895ba30b8cc1996bd2d42d213
Michelle Tan <michelletanjm@gmail.com>
2016-08-31 23:35:44 +0800

    Edit create journeys page

--- browser/journey-create/journey-create.controller.js
+++ browser/journey-create/journey-create.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
-app.controller('JourneyCreateCtrl', function($scope, $state, $stateParams, DatabaseFactory, FacebookFactory, $rootScope) {
+app.controller('JourneyCreateCtrl', function($scope, $rootScope, $state, $stateParams, DatabaseFactory, FacebookFactory, PixabayFactory) {
     $scope.defaultPostPic = '/images/landing-feature2.jpg';
 
     $scope.todayDate = new Date();
 
     $scope.countries = [{name: 'Malaysia'}, {name: 'Thailand'}, {name: 'England'}];
 
-    $scope.posts = []; // <-- Ten will provide some async function here
+    $scope.posts = [{}, {},{},{}]; // <-- Ten will provide some async function here
 
     $scope.selectedPosts = [];
 
@@ -17,10 +17,18 @@ app.controller('JourneyCreateCtrl', function($scope, $state, $stateParams, Datab
         $scope.journeyName = name;
     }
 
-    $scope.journeySource = "";
+    $scope.journeyCoverCountry = $scope.countries[0].name;
+
+    $scope.getJourneyCoverPhoto = function() {
+
+        PixabayFactory.getCountryImgUrl($scope.journeyCoverCountry)
+        .then(function(url){
+            $scope.journeyCoverPhoto = url;
+        });
+    }
 
     $scope.createJourney = function(){
         // Michelle: You can call createJourney, but you have to make sure to pass the correct journeyName, journeySource, and selectedPosts (an array of posts the user wants)
-    	DatabaseFactory.createJourney($rootScope.userId, $scope.journeyName, $scope.selectedPosts, $scope.journeySource);
-   	}
+        DatabaseFactory.createJourney($rootScope.userId, $scope.journeyName, $scope.selectedPosts, $scope.journeyCoverPhoto);
+    }
 });
