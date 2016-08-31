27c3d1e83ab9b8a12ce09a00cfa31c201a779d35
Michelle Tan <michelletanjm@gmail.com>
2016-08-31 22:27:19 +0800

    Add journey name field

--- browser/journey-create/journey-create.controller.js
+++ browser/journey-create/journey-create.controller.js
@@ -5,14 +5,14 @@ app.controller('JourneyCreateCtrl', function($scope, $state, $stateParams, Datab
 
     $scope.todayDate = new Date();
 
-    $scope.countries = [{}, {}, {}];
+    $scope.countries = [{name: 'Malaysia'}, {name: 'Thailand'}, {name: 'England'}];
 
     $scope.posts = []; // <-- Ten will provide some async function here
 
     $scope.selectedPosts = [];
 
     $scope.journeyName = "";
-    
+
     $scope.updateJourneyName = function(name){
         $scope.journeyName = name;
     }
@@ -20,7 +20,7 @@ app.controller('JourneyCreateCtrl', function($scope, $state, $stateParams, Datab
     $scope.journeySource = "";
 
     $scope.createJourney = function(){
-        // Michelle: You can call createJourney, but you have to make sure to pass the correct journeyName, journeySource, and selectedPosts (an array of posts the user wants)  
+        // Michelle: You can call createJourney, but you have to make sure to pass the correct journeyName, journeySource, and selectedPosts (an array of posts the user wants)
     	DatabaseFactory.createJourney($rootScope.userId, $scope.journeyName, $scope.selectedPosts, $scope.journeySource);
    	}
 });
