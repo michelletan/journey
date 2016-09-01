cfc41fc01ab648b9ab07ce4d48274c71ff44f792
superburrito <chuayaohui@gmail.com>
2016-09-01 19:59:47 +0800

    undid merge problems, restored journey journeys and navbar ctrl to earlier state

--- browser/journeys/journeys.controller.js
+++ browser/journeys/journeys.controller.js
@@ -1,22 +1,27 @@
 'use strict';
 
 app.controller('JourneysCtrl', function($scope, $state, $stateParams, DatabaseFactory, $rootScope){
-	$scope.userId = $rootScope.userId;
+    // Redirect users if they haven't logged in
+    if($rootScope.userId == null) $state.go('landing');
+
+    $scope.userId = $rootScope.userId;
     $scope.userName = $rootScope.userName;
     $scope.userSource = $rootScope.userSource;
-	// DatabaseFactory.getAllJourneys($rootScope.userId)
-	// .then(function(allUserData){
-    //     console.log("Journey to be displayed are:", allUserData.journeys);
-    //     var journeys = allUserData.journeys;
-    //     journeys.map(function(journey){
-    //         journey.created = journey.created.slice(6,11);
-    //         return journey;
-    //     })
-    //     $scope.journeys = journeys;
-	// });
-	$scope.journeys = [];
+    DatabaseFactory.getAllJourneys($rootScope.userId)
+    .then(function(allUserData){
+        var journeys = allUserData.journeys;
+        journeys = journeys.map(function(journey){
+            journey.posts = journey.posts.map(function(post){
+                post.created = post.created.slice(5,10);
+                return post;
+            })
+            return journey; 
+        }) 
+        console.log("Journeys to be displayed are:", journeys);
+        $scope.journeys = journeys;
+    });
 
-	$scope.defaultUserId = 1;
+    $scope.defaultUserId = 1;
     $scope.defaultUserPic = '/images/user.png';
     $scope.defaultUserName = 'User';
     $scope.defaultCoverUrl = '/images/landing-feature2.jpg';
