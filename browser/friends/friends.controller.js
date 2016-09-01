7b77a2a4e5ae754d0c56acf6afc9d2ca9fc8c384
Michelle Tan <michelletanjm@gmail.com>
2016-09-01 18:35:00 +0800

    Change indication for selected post

--- browser/friends/friends.controller.js
+++ browser/friends/friends.controller.js
@@ -5,10 +5,9 @@ app.controller('FriendsCtrl', function($scope, $state, $stateParams) {
     $scope.defaultUserPic = '/images/user.png';
 	$scope.defaultUserName = 'User';
 
-$scope.friends = [];
-    // $scope.friends = [
-    //     {name: 'Abc'}, {name: 'daffff'}, {name: 'eeeee'}, {name: 'ffff'}, {name: 'ggg'}, {name: 'Ahhhbc'}, {name: 'iiii'}, {name: 'jj'}
-    // ];
+    $scope.friends = [
+        {name: 'Abc'}, {name: 'daffff'}, {name: 'eeeee'}, {name: 'ffff'}, {name: 'ggg'}, {name: 'Ahhhbc'}, {name: 'iiii'}, {name: 'jj'}
+    ];
 
     $scope.inviteFriend = inviteFriend;
 
