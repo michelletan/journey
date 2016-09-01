52c5fe69340f0795214b969458ced42607a5c9bd
Michelle Tan <michelletanjm@gmail.com>
2016-09-02 00:14:48 +0800

    Update routing

--- browser/landing/landing.controller.js
+++ browser/landing/landing.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
-app.controller('LandingCtrl', function($scope, $state, $stateParams, FacebookFactory) {
+app.controller('LandingCtrl', function($scope, $rootScope, $state, $stateParams, FacebookFactory) {
+    // If logged in, redirect to home
+    if($rootScope.userId !== null) $state.go('home');
+
     $scope.spreadImagePath = '/images/landing-spread.jpg';//'https://dl.dropboxusercontent.com/s/34vnqzdl7k1x6ak/spread.jpg?dl=0';
 
     $scope.imageFeature1Path = '/images/landing-feature1.jpg';
