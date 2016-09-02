55705ea62cba8536d7e17177b53342a3fda47118
Michelle Tan <michelletanjm@gmail.com>
2016-09-02 22:28:27 +0800

    Fix create journey bug

--- browser/journey-create/country-post.filter.js
+++ browser/journey-create/country-post.filter.js
@@ -4,15 +4,15 @@
 // property for each post object, if the post object
 // contains a country name from given list of country names.
 app.filter('countryPost', function() {
-	if(posts == undefined){
-		return
-	}else{
-		return function(posts, countryNames) {
+	return function(posts, countryNames) {
+		if (posts) {
 			return posts.map(function(post) {
 				post.isVisible = (countryNames.indexOf(post.country) > -1);
 				post.isSelected = post.isSelected && post.isVisible;
 				return post;
 			});
-		};
-	}
+		} else {
+			return [];
+		}
+	};
 });
