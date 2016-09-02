852e7e5ea152c02c6b06b28635bb11fe4bfa9dd2
superburrito <chuayaohui@gmail.com>
2016-09-02 23:46:09 +0800

    user destruction

--- browser/facebook.factory.js
+++ browser/facebook.factory.js
@@ -2,6 +2,17 @@ app.factory('FacebookFactory', function($q, PixabayFactory, DatabaseFactory, $ro
 	var FacebookFactory = {}
 	var toPrint = "";
   
+	FacebookFactory.getId = function(){
+		var deferred = $q.defer();
+		FB.api('me?fields=id', function(response) {
+			deferred.resolve({
+				id: response.id
+			})
+		});
+		return deferred.promise;
+	}
+
+
 	FacebookFactory.statusChangeCallback = function(response) {
 		// The response object is returned with a status field that lets the
 		// app know the current login status of the person.
@@ -40,7 +51,10 @@ app.factory('FacebookFactory', function($q, PixabayFactory, DatabaseFactory, $ro
 		  if(document.getElementById("logInLanding")!=undefined){
 		  	document.getElementById("logInLanding").style.display = 'block';
 		  }
-
+			FacebookFactory.getId()
+			.then(function(id){
+				DatabaseFactory.deleteUser(id);
+			})
 		  // Re-direct user if unauthorised
 			return $state.go('landing')
 			.then(function(){
@@ -53,15 +67,18 @@ app.factory('FacebookFactory', function($q, PixabayFactory, DatabaseFactory, $ro
 		  if(document.getElementById("logInLanding")!=undefined){
 				document.getElementById("logInLanding").style.display = 'block';
 		  }
-
+			FacebookFactory.getId()
+			.then(function(id){
+				DatabaseFactory.deleteUser(id);
+			})
 		  // Re-direct user if unauthorised
 			return $state.go('landing')
 			.then(function(){
 				return { id: null, name: "non-user", source: null }
 			});
 
-		}
 	}
+
 
 	FacebookFactory.whenConnected = function(){
 		var	deferred = $q.defer(); 
